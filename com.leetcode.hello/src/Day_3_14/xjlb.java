f2bfde489be6df5d5485fe9122b483110d9335a1
wtangle <2068181694@qq.com>
2024-03-14 22:30:06 +0800

    2024/3/14

---
+++ com.leetcode.hello/src/Day_3_14/xjlb.java
@@ -0,0 +1,22 @@
+package Day_3_14;
+//相交链表
+public class xjlb {
+    /*
+    ListNode LA=headA;ListNode LB=headB;
+        Set<ListNode> sy=new HashSet<ListNode>();
+
+        for(;LA!=null;){
+            sy.add(LA);
+            LA=LA.next;
+        }
+        for(;LB!=null;){
+            if(sy.contains(LB))
+            return LB;
+            LB=LB.next;
+        }
+        return null;
+     */
+    /*
+
+     */
+}
