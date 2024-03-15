fe4ea245e4c69b7319f60fce71a1aabdd00113ce
wtangle <2068181694@qq.com>
2024-03-15 13:52:32 +0800

    2024/3/15

---
+++ com.leetcode.hello/src/Day_3_14/hxlb.java
@@ -0,0 +1,31 @@
+package Day_3_14;
+//环形链表
+
+
+public class hxlb {
+    public static void main(String[] args) {
+        //哈希表
+        // Set<ListNode> table=new HashSet<ListNode>();
+        // for(;head!=null;head=head.next){
+        //     if(!table.add(head))
+        //     return true;
+        // }
+        // return false;
+
+        //快慢指针
+        /*if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        do{
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }while (slow != fast);
+        return true;
+        */
+    }
+}
