0fb9e7df4bd13566c6bc6d33fa5b03456d8e1551
wtangle <2068181694@qq.com>
2024-07-09 18:12:42 +0800

    2024/7/9

--- com.leetcode.hello/src/Day_7_9/ascii.java
+++ com.leetcode.hello/src/Day_7_9/ascii.java
@@ -2,9 +2,9 @@ package Day_7_9;
 
 public class ascii {
     public static void main(String[] args) {
-        // 'a' + 10 输出整数
+        // 'a' + 10 输出的是整数
         System.out.println('a'+10);
-        // 'a' 输出字符
+        // 'a' 输出的是字符
         System.out.println('a');
     }
 }
